21e3f8381cebe4f6f3ff0a31849dc8ddcea4bc98
Michael Yabut <spanishiwa@gmail.com>
2016-11-20 21:16:08 -0800

    added variable names and loops for wordLadder

---
+++ wordLadder.js
@@ -0,0 +1,15 @@
+function ladderLength(beginWord, endWord, wordList) {
+  let wordBranches = [];
+  let idx = beginWord.length;
+  let word = beginWord;
+
+  while (idx < beginWord.length) {
+    for (let i = 0; i < 26; i += 1) {
+
+    }
+
+    idx += 1;
+  }
+}
+
+console.log(ladderLength("hit", "cog", ["hot","dot","dog","lot","log"]));
