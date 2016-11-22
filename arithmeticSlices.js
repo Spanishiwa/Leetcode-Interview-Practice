d5ba8c67caecfb13f87a9b5f99d8110e53a96929
Michael Yabut <spanishiwa@gmail.com>
2016-11-21 23:46:55 -0800

    arithmeticSlices first submission faster than 30%

--- arithmeticSlices.js
+++ arithmeticSlices.js
@@ -1,11 +1,28 @@
 function arithmeticSlices(nums) {
   let count = 0;
+  let len = nums.length;
 
-  for (let i = 0; i < nums.length - 2; i += 1) {
-    let slice = nums.slice(i, i+3);
+  for (let i = 0; i < len - 2; i += 1) {
+    let endSlice = (i+3);
+    let slice = nums.slice(i, endSlice);
+
+    while (endSlice <= len && arithmetic(slice)) {
+      count += 1;
+      endSlice += 1;
+      slice = nums.slice(i, endSlice);
+    }
+    endSlice = (i+3);
   }
+
+  return count;
 }
 
-[1,2,3,4] 4
-[1,2,3]
-[2,3,4]
+function arithmetic(nums) {
+  let diff1 = (nums[1] - nums[0]);
+  let diff2 = (nums[nums.length - 1] - nums[nums.length - 2]);
+
+  return (diff2 === diff1);
+}
+
+// console.log(arithmeticSlices([1, 2, 3, 4]));
+console.log(arithmeticSlices([1,2,3,8,9,10]));
