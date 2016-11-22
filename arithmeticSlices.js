7363b26bfbe17a6e3657c95b597102113f5565e7
Michael Yabut <spanishiwa@gmail.com>
2016-11-21 22:51:49 -0800

    Arithmetic slice and queueReconstruction problems first attempt

---
+++ arithmeticSlices.js
@@ -0,0 +1,11 @@
+function arithmeticSlices(nums) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length - 2; i += 1) {
+    let slice = nums.slice(i, i+3);
+  }
+}
+
+[1,2,3,4] 4
+[1,2,3]
+[2,3,4]
