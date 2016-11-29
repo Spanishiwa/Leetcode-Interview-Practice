f5fa8132c6f146408306a9b0f99a72656ae1c43a
Michael Yabut <spanishiwa@gmail.com>
2016-11-28 23:32:17 -0800

    queue reconstruction first submission

--- queueReconstruction.js
+++ queueReconstruction.js
@@ -1,5 +1,17 @@
 function reconstructQueue(people) {
+  let queue = [];
 
+  let sorted = people.sort(function(p, c) {
+    return (c[0] - p[0]);
+  });
+
+  sorted.forEach(function(person){
+    queue.splice(person[1], 0, person);
+  });
+
+  return queue;
 }
 
-console.log(reconstructQueue([[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]));
+let test1 = [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]];
+let result1 = [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]];
+console.log(`result is ${reconstructQueue(test1)}, it should be ${result1}`);
