d97cc332110411536f34f2ff803fc7266f0462a4
Michael Yabut <spanishiwa@gmail.com>
2016-11-28 23:39:05 -0800

    queue reconstruction first submission accepted

--- queueReconstruction.js
+++ queueReconstruction.js
@@ -2,7 +2,7 @@ function reconstructQueue(people) {
   let queue = [];
 
   let sorted = people.sort(function(p, c) {
-    return (c[0] - p[0]);
+    return (c[0] === p[0]) ? p[1] - c[1] : c[0] - p[0];
   });
 
   sorted.forEach(function(person){
@@ -13,5 +13,9 @@ function reconstructQueue(people) {
 }
 
 let test1 = [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]];
+let test2 = [[9,0],[7,0],[1,9],[3,0],[2,7],[5,3],[6,0],[3,4],[6,2],[5,2]];
 let result1 = [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]];
-console.log(`result is ${reconstructQueue(test1)}, it should be ${result1}`);
+let result2 = [[3,0],[6,0],[7,0],[5,2],[3,4],[5,3],[6,2],[2,7],[9,0],[1,9]];
+
+console.log(`result 1 is ${reconstructQueue(test1)}, it should be ${result1}`);
+console.log(`result 2 is ${reconstructQueue(test2)}, it should be ${result2}`);
