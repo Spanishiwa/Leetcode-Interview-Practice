f53757775bd1cb247647c00491f605cccc0671c7
Michael Yabut <spanishiwa@gmail.com>
2016-12-12 22:40:50 -0800

    find Kth frequent element leetcode first submission

---
+++ kthFrequentElement.js
@@ -0,0 +1,16 @@
+function findKthLargest(nums, k) {
+  const freqObj = {};
+
+  for (let i = 0; i < nums.length; i +=1) {
+    if (freqObj[nums[i]]) {
+      freqObj[nums[i]] += 1;
+    }
+    else {
+      freqObj[nums[i]] = 1;
+    }
+  }
+
+  return Object.keys(freqObj).sort((p, c) => {
+      return (freqObj[c] - freqObj[p]);
+  }).slice(0, k).map(j => parseInt(j));
+}
