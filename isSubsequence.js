134f4a5bc5a4c1da34ea1ccdca815bed0ede1504
Michael Yabut <spanishiwa@gmail.com>
2016-12-12 23:42:50 -0800

    speed refactor for isSubsequence

--- isSubsequence.js
+++ isSubsequence.js
@@ -1,11 +1,16 @@
 function isSubsequence(s, t) {
-  const lettersToMatch = s.split('');
+  let lettersToMatch = "";
+  let idx = 0;
 
   for (let i = 0; i < t.length; i += 1) {
-    if (t[i] === lettersToMatch[0]) {
-      lettersToMatch.shift(1);
+    if (t[i] === s[idx]) {
+      lettersToMatch += t[i];
+      idx += 1;
+    }
+    else if (lettersToMatch === s) {
+      return true;
     }
   }
 
-  return (lettersToMatch.length === 0);
+  return (lettersToMatch === s);
 }
